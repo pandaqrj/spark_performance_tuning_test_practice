1b3b3123815eec8bdde7d9be07b82a838c7f3f56
panda <pandaqrj@live.com>
2021-03-04 19:23:46 +0800

    Update spark_sql_hint.py

--- spark_sql_hint.py
+++ spark_sql_hint.py
@@ -2,7 +2,11 @@ from pyspark.sql import SparkSession
 
 
 def exe_spark():
-    pass
+    spark = SparkSession.builder \
+        .master("local[*]") \
+        .appName("sparksql hint test") \
+        .getOrCreate()
+    sc = spark.sparkContext
 
 
 if __name__ == "__main__":
