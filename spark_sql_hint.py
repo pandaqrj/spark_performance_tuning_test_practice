5d8b9058a00572b02d1e901bca695f24d2bb5619
panda <pandaqrj@live.com>
2021-03-04 19:28:12 +0800

    Update spark_sql_hint.py

--- spark_sql_hint.py
+++ spark_sql_hint.py
@@ -4,9 +4,17 @@ from pyspark.sql import SparkSession
 def exe_spark():
     spark = SparkSession.builder \
         .master("local[*]") \
-        .appName("sparksql hint test") \
+        .appName("spark sql hint test") \
         .getOrCreate()
-    sc = spark.sparkContext
+    # sc = spark.sparkContext
+    """
+        SELECT /*+ COALESCE(3) */ * FROM t
+        SELECT /*+ REPARTITION(3) */ * FROM t
+        SELECT /*+ REPARTITION(c) */ * FROM t
+        SELECT /*+ REPARTITION(3, c) */ * FROM t
+        SELECT /*+ REPARTITION_BY_RANGE(c) */ * FROM t
+        SELECT /*+ REPARTITION_BY_RANGE(3, c) */ * FROM t
+    """
 
 
 if __name__ == "__main__":
