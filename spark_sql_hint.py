0045fd6327e68979c8f77bb5dac8f2f3d9b73f9c
panda <pandaqrj@live.com>
2021-03-04 21:07:00 +0800

    Update spark_sql_hint.py
    sparksql的hint

--- spark_sql_hint.py
+++ spark_sql_hint.py
@@ -23,16 +23,18 @@ def exe_spark():
     # 重分区，不会shuffle, 是将同一个节点上的多个分区进行重分区
     # 一般用于减少分区从而减少spark-sql产生的大量小文件
     # 但是在数据量较大的情况下会降低计算速度
-    spark.sql("""
-        SELECT /*+ COALESCE(3) */ * FROM t
-    """).show()
+    spark.sql(
+        """
+            SELECT /*+ COALESCE(3) */ * FROM t
+        """).show()
 
     # 重分区，会shuffle数据以实现负载均衡，一般用于增加分区数以提高并行度
     # 在执行join操作或者cache方法之前调用会产生不错的效果
     # 但是要注意shuffle的开销
-    spark.sql("""
-        SELECT /*+ REPARTITION(3) */ * FROM t
-    """).show()
+    spark.sql(
+        """
+            SELECT /*+ REPARTITION(3) */ * FROM t
+        """).show()
 
 
 if __name__ == "__main__":
